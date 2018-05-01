7013076042877969a51aa0b588a5ba0675c729b1
Ryan Carter <me@rncrtr.in>
2018-05-01 06:03:24 +0000

    init

--- app/app.js
+++ app/app.js
@@ -1,11 +1,10 @@
 'use strict';
-
+/* global angular */
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.view2'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
